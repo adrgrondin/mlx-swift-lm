9327afc12c601ca1e3e316253aca659150707449
Adrien Grondin <adrien_grondin@icloud.com>
2026-05-29 11:01:55 -0400

    Update mlx-swift to v0.31.3_prism

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
             targets: ["IntegrationTestHelpers"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.31.3")),
+        .package(url: "https://github.com/PrismML-Eng/mlx-swift", branch: "v0.31.3_prism"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "604.0.0"),
     ],
     targets: [
